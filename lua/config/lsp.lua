a23cfbac736aaeadfeb002d5dbd5ed2d90e5b016
pop-ecx <oliver.mulwa@pm.me>
2024-12-19 20:43:36 +0300

    added docker ls and I use neovide now

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,8 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig.lua_ls.setup({capabilities = capabilities})
-lspconfig.zls.setup({capabilities = capabilities})
+lspconfig.lua_ls.setup({capabilities = capabilities })
+lspconfig.zls.setup({ capabilities = capabilities })
 lspconfig.phpactor.setup({})
 -- lspconfig.pylyzer.setup({})
-lspconfig.dockerls.setup({})
-
+lspconfig.dockerls.setup({ capabilities = capabilities })
---
+++ lua/config/neovide.lua
@@ -0,0 +1,4 @@
+if vim.g.neovide then
+    vim.o.guifont = "FiraMono Nerd Font:h7"
+    vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
