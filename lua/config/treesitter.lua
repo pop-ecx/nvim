8db1c599707826bc8d39d898a0d1748480539e22
pop-ecx <oliver.mulwa@pm.me>
2024-12-21 21:12:43 +0300

    treesitter parsers

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,3 +3,4 @@ require("nvim-treesitter.configs").setup({
   highlight = { enable = true },
   indent = { enable = true },
 })
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
