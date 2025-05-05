08e20a22f276ba0688e3ecbbf78ae28b97005300
pop-ecx <oliver.mulwa@pm.me>
2025-05-05 21:45:24 +0300

    completions update

--- lua/config/completions.lua
+++ lua/config/completions.lua
@@ -1,25 +1,37 @@
-local cmp = require("cmp")
-require("luasnip.loaders.from_vscode").lazy_load()
+local cmp = require('cmp')
+require('luasnip.loaders.from_vscode').lazy_load()
+
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-o>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-o>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-x>'] = cmp.mapping.confirm({ select = true }),
+  }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = "copilot", group_index = 2 },
+    { name = 'nvim_lsp'},
+    { name = 'copilot', group_index = 2 },
     { name = 'luasnip' },
-  },{
+  }, {
     { name = 'buffer' },
   }),
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.menu = ({
+        nvim_lsp = '[LSP]',
+        copilot = '[Copilot]',
+        luasnip = '[Snippet]',
+        buffer = '[Buffer]',
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
