e4b2454a2b200d692d1d863ecd9668379dbd3608
pop-ecx <oliver.mulwa@pm.me>
2026-03-05 12:26:37 +0300

    made some lsp changes involving Zig, python, and added ocaml lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,10 +1,40 @@
 -- local lspconfig = require("lspconfig")
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+vim.lsp.config("zls", {
+    settings = {
+        zls = {
+            zig_exe_path = "/home/m3lk0r/.cache/zig/p/N-V-__8AAN5NhBR0oTsvnwjPdeNiiDLtEsfXRHd1fv-R3TOv/zig",
+        },
+    },
+})
+
+vim.lsp.config("yls", {
+    settings = {
+        yls = {
+            yls_exe_path = "/home/m3lk0r/.local/share/nvim/mason/bin/yls",
+        },
+    },
+})
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
+})
+
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("zls")
-vim.lsp.enable("pyright")
+vim.lsp.enable("ruff")
 vim.lsp.enable("dockerls")
 vim.lsp.enable("phpactor")
 vim.lsp.enable("superhtml")
---vim.lsp.enable("yls")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("docker_compose_language_service")
+vim.lsp.enable("lemminx")
+vim.lsp.enable("yls")
+vim.lsp.enable("ocamllsp")
