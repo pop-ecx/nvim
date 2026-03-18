ef32fe609ca4dbb87e1c65b3e760c352d4683831
pop-ecx <oliver.mulwa@pm.me>
2026-03-18 13:01:48 +0300

    skip simple edits confirmation in oil

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -99,6 +99,7 @@ local plugins = {
       view_options = {
         show_hidden = true,
       },
+      skip_confirm_for_simple_edits = true,
     },
   -- Optional dependencies
   -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
